2029c1622c4a3101e010b26a6e18bdc6a24e153f
Shah Sahil <47909424+shahsahil713@users.noreply.github.com>
2020-09-24 13:00:51 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>{mylover}</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>sahil</h2>
     </div>
   );
 }
